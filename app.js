571faec709c08b5d69e4aecba2f5f47479397d3d
f88643 <f88643@hotmail.com>
2023-12-11 17:41:00 +0800

    header & footer

--- app.js
+++ app.js
@@ -5,22 +5,29 @@ const session = require('express-session')
 
 const routes = require('./routes')
 const db = require('./models')
+const passport = require('./config/passport')
+
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize()) // 初始化 Passport
+app.use(passport.session()) // 啟動 session 功能
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages')  // 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
