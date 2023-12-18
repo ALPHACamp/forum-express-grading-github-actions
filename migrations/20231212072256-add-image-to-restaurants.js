5e8b4bce5e74bb486146adc730abc0fd1a00e7ce
f88643 <f88643@hotmail.com>
2023-12-18 14:20:12 +0800

    test

--- migrations/20231212072256-add-image-to-restaurants.js
+++ migrations/20231212072256-add-image-to-restaurants.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('restaurants', 'image', {
+    await queryInterface.addColumn('Restaurants', 'image', {
       type: Sequelize.STRING
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('restaurants', 'image')
+    await queryInterface.removeColumn('Restaurants', 'image')
   }
 }
