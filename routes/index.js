61b9d31bb9dda8f1873bd800ebb1b846afa07cc1
f88643 <f88643@hotmail.com>
2023-12-22 21:15:45 +0800

    following function

--- routes/index.js
+++ routes/index.js
@@ -26,13 +26,11 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/', (req, res) => {
-  res.redirect('/restaurants')
-})
 
 router.delete('/comments/:id', authenticated, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
@@ -43,6 +41,10 @@ router.post('/favorite/:restaurantId', authenticated, userController.addFavorite
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 
+router.post('/following/:userId', authenticated, userController.addFollowing)
+router.delete('/following/:userId', authenticated, userController.removeFollowing)
+
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
 module.exports = router
