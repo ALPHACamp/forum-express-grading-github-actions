895e9121c3972aaf79ffe74d7c6ddee870a032c6
Tim2319 <destiny65740@gmail.com>
2024-01-25 13:52:18 +0800

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
+
 const passport = require('../config/passport')
+
 const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/comment-controller')
+
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+
 const { generalErrorHandler } = require('../middleware/error-handler')
 const upload = require('../middleware/multer')
 
@@ -18,17 +22,18 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
-// restaurants & Dashboard
+// Users
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+// restaurants & Dashboard & feeds
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 // comments
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
-// Users
-router.get('/users/:id/edit', authenticated, userController.editUser)
-router.get('/users/:id', authenticated, userController.getUser)
-router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
