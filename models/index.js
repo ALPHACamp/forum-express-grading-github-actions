eee802e9d2b4f81c4ded93fb8d50204609434691
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-03-11 14:44:58 +0800

    feat: add user model

--- models/index.js
+++ models/index.js
@@ -17,18 +17,17 @@ if (config.use_env_variable) {
 }
 
 // 動態引入其他 models
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
   })
-  .forEach(file => {
+  .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
     db[model.name] = model
   })
 
 // 設定 Models 之間的關聯
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db)
   }
