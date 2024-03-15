2c65a47241109c4195aeb5f3d6ed9d205e55d27f
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-03-15 14:30:30 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,10 +1,31 @@
-const { Category } = require('../models')
+const db = require('../models')
+const Categorys = db.Category
+
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({
+    return Categorys.findAll({
       raw: true
     })
       .then((categories) => res.render('admin/categories', { categories }))
+      .catch((err) => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { newCategory } = req.body
+    if (!newCategory) throw new Error('Category name is required!')
+    return Categorys.findOne({
+      where: { name: newCategory },
+      raw: true
+    })
+      .then((category) => {
+        if (category) throw new Error('Category already exists!')
+        return Categorys.create({
+          name: newCategory
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '成功新增類別！')
+        return res.redirect('/admin/categories')
+      })
       .catch((err) => next(err))
   }
 }
