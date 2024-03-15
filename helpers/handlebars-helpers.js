4afaac1e7267b161ed75a54270b937a1be0cbf93
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-03-15 13:50:07 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -3,5 +3,8 @@ const dayjs = require('dayjs')
 /*輸出東西提共給handlebars template{{}}引用************************************* */
 
 module.exports = {
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
