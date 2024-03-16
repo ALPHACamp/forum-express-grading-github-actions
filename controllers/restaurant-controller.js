3eac799ab69067be537ce224c6d43da0cdfe81d6
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-03-16 21:45:13 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,7 +3,7 @@ const Restaurants = db.Restaurant
 const Category = db.Category
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
+  getRestaurants: (req, res, next) => {
     Restaurants.findAll({
       raw: true,
       nest: true,
@@ -15,6 +15,19 @@ const restaurantController = {
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', { restaurants: data })
+      })
+      .catch((err) => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    const restaurantId = req.params.id
+    Restaurants.findByPk(restaurantId, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then((restaurant) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return res.render('restaurant', { restaurant: restaurant })
       })
       .catch((err) => next(err))
   }
