d9a10a666a14ee6bc3865b2f73f0a8de57d782fc
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-03-18 10:03:11 +0800

    finish R02 test

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -20,13 +20,12 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     const restaurantId = req.params.id
-    Restaurants.findByPk(restaurantId)
+    return Restaurants.findByPk(restaurantId)
       .then((restaurant) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         return Restaurants.increment('viewCounts', { where: { id: restaurantId } })
       })
       .then(() => {
-        // 查询餐厅数据并渲染
         return Restaurants.findByPk(restaurantId, {
           raw: true,
           nest: true,
@@ -34,21 +33,20 @@ const restaurantController = {
         })
       })
       .then((restaurant) => {
-        // 渲染餐厅信息
         res.render('restaurant', { restaurant: restaurant })
       })
       .catch((err) => next(err))
   },
   getDashboard: (req, res, next) => {
     const restaurantId = req.params.id
-    Restaurants.findByPk(restaurantId, {
+    return Restaurants.findByPk(restaurantId, {
       raw: true,
       nest: true,
       include: [Category]
     })
       .then((restaurant) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('dashboard', { restaurant })
+        res.render('dashboard', { restaurant: restaurant })
       })
       .catch((err) => next(err))
   }
